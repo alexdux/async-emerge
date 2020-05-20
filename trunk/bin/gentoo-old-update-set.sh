3a9caae236f3f3b50214ff3488ba301d1f128faf
Alex Dubenetsky <alexdu.com.ae@gmail.com>
2020-05-21 00:22:23 +0300

    gentoo-old-update bump 1.2

--- trunk/bin/gentoo-old-update-set.sh
+++ trunk/bin/gentoo-old-update-set.sh
@@ -5,12 +5,12 @@
 #	1.1 Wed May 20 21:40:08 2020
 #		- up to 3 user params to emerge added
 #		- default -ask removed
+#	1.2 Thu May 21 00:20:33 2020
+#		- remove interactive editing the list
 
-EDITOR="mcedit"
 TMP_FILE="/tmp/${1}.txt"
 
-emerge --pretend --emptytree --quiet --columns --color\=n "${1}" | awk -F ' ' '{print $2}' > "${TMP_FILE}" && \
-${EDITOR} "${TMP_FILE}" && \
+emerge --pretend --emptytree --quiet --columns --color\=n "${1}" | sed -e '/^$/,$d' | awk -F ' ' '{print $2}' > "${TMP_FILE}" && \
 emerge --verbose --oneshot --nodeps --keep-going "${@:2}" $(cat "${TMP_FILE}")
 
 
