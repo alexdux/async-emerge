3535f28b81c8026f4154ec0a09b7825770e863c1
Alex Dubenetsky <alexdu.com.ae@gmail.com>
2020-05-21 12:34:34 +0300

    added echo's

--- trunk/bin/gentoo-old-update-set.sh
+++ trunk/bin/gentoo-old-update-set.sh
@@ -10,10 +10,16 @@
 #		- remove --emptytree
 #	1.3 Thu May 21 12:08:47 2020
 #		- remove --verbose
+#	1.4 Thu May 21 12:14:33 2020
+#		- echo added
 
 TMP_FILE="/tmp/${1}.txt"
 
+echo "<<< (1/2) Request packets ordered list from portage for set \"${1}\". Please wait ... >>>"
 emerge --pretend "${2}" --quiet --columns --color\=n "${1}" | sed -e '/^$/,$d' | cut -c8- | cut -d' ' -f1 > "${TMP_FILE}" && \
+echo "<<< (2/2) Update the list without dependencies checking. It takes a while ...          >>>" && \
+echo "<<<       Please use Ctrl+C to stop; --verbose/--queit for control output details;     >>>" && \
+echo "<<<       and \"nonup ... &\" and \"tail -f nohup.out\" to run in background.          >>>" && \
 emerge --oneshot --nodeps --keep-going "${@:3}" $(cat "${TMP_FILE}")
 
 
--- trunk/bin/gentoo-old-update-take-a-chance.sh
+++ trunk/bin/gentoo-old-update-take-a-chance.sh
@@ -6,18 +6,35 @@
 #		- --update/--emptytree added
 #	1.2 Thu May 21 11:55:06 2020
 #		- toolchain update added
+#	1.3 Thu May 21 12:24:54 2020
+#		- system echo added
 
 # TODO: detect emerge exit code
 
-emerge --oneshot --nodeps gcc
-emerge --oneshot --nodeps binutils
-emerge --oneshot --nodeps glibc
-emerge --depclean --nodeps gcc binutils glibc
+[ "$RC_GOT_FUNCTIONS" ] || . /etc/init.d/functions.sh
+
+          einfo "1/12. Update gcc ..." && \
+emerge --oneshot --nodeps gcc && \
+eend 0 && einfo "2/12. Update binutils ..." && \
+emerge --oneshot --nodeps binutils && \
+eend 0 && einfo "3/12. Update glibc ..." && \
+emerge --oneshot --nodeps glibc && \
+eend 0 && einfo "4/12. Remove old version of the toolchains ..." && \
+emerge --depclean --nodeps gcc binutils glibc && \
+eend 0 && einfo "5/12. Update profile ..." && \
 . /etc/profile && \
+eend 0 && einfo "6/12. Rebuild system set (pass 1/3) ..." && \
 gentoo-old-update-set.sh system --emptytree "${@}" && \
+eend 0 && einfo "7/12. Rebuild system set (pass 2/3) ..." && \
 gentoo-old-update-set.sh system --emptytree "${@}" && \
+eend 0 && einfo "8/12. Rebuild system set (pass 3/3) ..." && \
 gentoo-old-update-set.sh system --emptytree "${@}" && \
+eend 0 && einfo "9/12. Rebuild world set (pass 1/3) ..." && \
 gentoo-old-update-set.sh world --emptytree "${@}" && \
+eend 0 && einfo "10/12. Rebuild world set (pass 2/3) ..." && \
 gentoo-old-update-set.sh world --emptytree "${@}" && \
+eend 0 && einfo "11/12. Rebuild world set (pass 3/3) ..." && \
 gentoo-old-update-set.sh world --emptytree "${@}" && \
-emerge --emptytree "${@}" @world 
+eend 0 && einfo "12/12. Rebuild world set with dependancies ..." && \
+emerge --emptytree "${@}" @world && \
+eend 0 
