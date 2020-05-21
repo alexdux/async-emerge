1924f12ad0da1452b3ab3328abac0ca46c2a6e3b
Alex Dubenetsky <alexdu.com.ae@gmail.com>
2020-05-21 12:41:06 +0300

    bump

--- trunk/bin/gentoo-old-update-take-a-chance.sh
+++ trunk/bin/gentoo-old-update-take-a-chance.sh
@@ -14,13 +14,13 @@
 [ "$RC_GOT_FUNCTIONS" ] || . /etc/init.d/functions.sh
 
  einfo "1/12. Update gcc ..." && \
-emerge --oneshot --nodeps gcc && \
+emerge --oneshot --nodeps "${@}" gcc && \
  einfo "2/12. Update binutils ..." && \
-emerge --oneshot --nodeps binutils && \
+emerge --oneshot --nodeps "${@}" binutils && \
  einfo "3/12. Update glibc ..." && \
-emerge --oneshot --nodeps glibc && \
+emerge --oneshot --nodeps "${@}" glibc && \
  einfo "4/12. Remove old version of the toolchains ..." && \
-emerge --depclean --nodeps gcc binutils glibc && \
+emerge --depclean --nodeps "${@}" gcc binutils glibc && \
  einfo "5/12. Update profile ..." && \
 . /etc/profile && \
  einfo "6/12. Rebuild system set (pass 1/3) ..." && \
