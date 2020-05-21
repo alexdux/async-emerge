00df477c1e922bc331fed58643b1f5884c37fb8a
Alex Dubenetsky <alexdu.com.ae@gmail.com>
2020-05-21 12:05:11 +0300

    bump

--- trunk/bin/gentoo-old-update-take-a-chance.sh
+++ trunk/bin/gentoo-old-update-take-a-chance.sh
@@ -4,14 +4,20 @@
 #		- initial
 #	1.1 Thu May 21 00:25:48 2020
 #		- --update/--emptytree added
+#	1.2 Thu May 21 11:55:06 2020
+#		- toolchain update added
 
 # TODO: detect emerge exit code
 
-gentoo-old-update-set.sh system --emptytree "${@}" && \
-gentoo-old-update-set.sh system --emptytree "${@}" && \
-(eselect gcc set 2 || true) && \
-(eselect binutils set 2 || true) && \
+emerge --oneshot --nodeps gcc
+emerge --oneshot --nodeps binutils
+emerge --oneshot --nodeps glibc
+emerge --depclean --nodeps gcc binutils glibc
 . /etc/profile && \
+gentoo-old-update-set.sh system --emptytree "${@}" && \
+gentoo-old-update-set.sh system --emptytree "${@}" && \
+gentoo-old-update-set.sh system --emptytree "${@}" && \
+gentoo-old-update-set.sh world --emptytree "${@}" && \
 gentoo-old-update-set.sh world --emptytree "${@}" && \
 gentoo-old-update-set.sh world --emptytree "${@}" && \
 emerge --emptytree "${@}" @world 
