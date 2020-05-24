9c549ef464477e7be579043cff500f0eb626adf2
Alex Dubenetsky <alexdu.com.ae@gmail.com>
2020-05-24 11:32:15 +0300

    1.6 bump

--- trunk/bin/gentoo-old-update-set.sh
+++ trunk/bin/gentoo-old-update-set.sh
@@ -15,15 +15,18 @@
 #		- COLLISION_IGNORE added
 #	1.5 Sat May 23 14:23:54 2020
 #		- tmp_file moved from /tmp/ to /var/tmp/portage
+#	1.6 Sun 24 May 2020 11:31:13 AM MSK
+#		- auto_unmask added
 
 TMP_FILE="/var/tmp/portage/${1}.txt"
+AU_PATH="/etc/portage/package*/"
+AU_FILE="/zzz_auto_unmask"
 
 echo "<<< (1/2) Request packets ordered list from portage for set \"${1}\". Please wait ... >>>"
-emerge --pretend "${2}" --quiet --columns --color\=n "${1}" | sed -e '/^$/,$d' | cut -c8- | cut -d' ' -f1 > "${TMP_FILE}"
+for i in ${AU_PATH}; do [[ -e ${i}${AU_FILE} ]] ||touch ${i}${AU_FILE}; done
+emerge --pretend ${2} --quiet --columns --color\=n ${1} | sed -e '/^$/,$d' | cut -c8- | cut -d' ' -f1 > "${TMP_FILE}"
 echo "<<< (2/2) Update the list without dependencies checking. It takes a while ...      >>>"
 echo "<<<       Please use <Ctrl>+<C> several times to stop;                             >>>"
 echo "<<<       --verbose/--queit for control output details;                            >>>"
 echo "<<<       and \"nonup ... &\" and \"tail -f nohup.out\" to run in background.          >>>"
-COLLISION_IGNORE="*" emerge --oneshot --nodeps --keep-going "${@:3}" $(cat "${TMP_FILE}")
-
-
+COLLISION_IGNORE="*" emerge --oneshot --nodeps --keep-going --autounmask=y --autounmask-continue=y --autounmask-write=y ${@:3} $(cat "${TMP_FILE}")
